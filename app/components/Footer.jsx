b06e451538699224565a48931c477fe986c0370f
adityatadas24 <adityatadas2000@gmail.com>
2024-12-24 21:48:12 +0530

    amrutam-task

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,13 +4,13 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex justify-center items-center w-full h-[60vh] bg-[#dce3dc] gap-5 flex-col max-sm:h-[100vh] max-sm:pt-24 max-sm:pb-24">
-
-      <div className="flex justify-start items-start flex-col max-sm:w-[70%] gap-3">
-        <h3 className="text-green-800 text-start font-semibold">Get in touch</h3>
+      
+      <div className="flex justify-start items-start flex-col max-sm:block max-sm:w-[70%]">
+        <h3 className="text-green-800 text-start py-2 font-semibold">Get in touch</h3>
         <p>support@amrutam.co.in</p>
-        <p>Amrutam Pharmaceuticals Pvt Ltd., chitragupt ganj, Nai Sadak, Lashkar, Gwalior - 474001</p>
+        <p className="py-2">Amrutam Pharmaceuticals Pvt Ltd., chitragupt ganj, Nai Sadak, Lashkar, Gwalior - 474001</p>
         <p>+91 9713171999</p>
-        <Image  src='/media.png' width='300' height='300' alt='media' />
+        <Image className="py-2"  src='/media.png' width='300' height='300' alt='media' />
         
       </div>
 
@@ -25,8 +25,8 @@ const Footer = () => {
         <li>For Businesses, Hotels and Resorts</li>
       </ul>
 
-      <div className="flex justify-start items-start flex-col max-sm:w-[70%] gap-3">
-        <h3 className="text-green-800 text-start font-semibold">Subscribe to our 
+      <div className="flex justify-start items-start flex-col  max-sm:block max-sm:w-[70%]">
+        <h3 className="text-green-800 text-start font-semibold py-3">Subscribe to our 
           Newsletter and join Amrutam Family today!</h3>
           <div className="flex justify-start items-center border-2 border-black rounded-full">
             <input className="bg-transparent placeholder:text-black" type="text" placeholder="Your Email Address" />
